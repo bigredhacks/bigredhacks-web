1fd7b56d9fefc4519baf9771423fb13d33d6938e
Austin Liu <atrix315@gmail.com>
2017-07-12 23:15:37 -0700

    removing deprecated and reference-free files
    more clean up
    
    
    cleanup

--- library/memoryCache.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var memoryCache = module.exports = function () {
-    var cache = {};
-    return {
-        get: function (key) { return cache[key]; },
-        set: function (key, val) { cache[key] = val; }
-    }
-};
