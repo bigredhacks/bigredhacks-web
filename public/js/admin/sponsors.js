8b051976752606e2f53fa2a6faf4c769f194bba0
Austin Liu <atrix315@gmail.com>
2017-07-29 15:59:01 -0700

    soylent  + sponsor csvs

--- public/js/admin/sponsors.js
+++ public/js/admin/sponsors.js
@@ -1,12 +1,12 @@
-function setDownloads(input){
-    $("#contactInfo").click(function(){
+function setDownloads(input, id, downloadName){
+    $("#" + id).click(function(){
         let csvContent = "data:text/csv;charset=utf-8,";
         csvContent += decodeURI(input);
 
         let encodedUri = encodeURI(csvContent);
         let link = document.createElement("a");
         link.setAttribute("href", encodedUri);
-        link.setAttribute("download", "bigredhacksParticipantInfo" + (new Date().getFullYear()) + ".csv");
+        link.setAttribute("download", downloadName);
         document.body.appendChild(link); // Required for FF
         link.click(); // This will download the data file named "my_data.csv".
     });
--- routes/admin/sponsors.js
+++ routes/admin/sponsors.js
@@ -1,6 +1,7 @@
 let fs = require('fs');
 let async = require('async');
 let mongoose = require('mongoose');
+let helper = require('../../util/routes_helper.js');
 mongoose.connect(process.env.COMPOSE_URI || process.env.MONGOLAB_URI || 'mongodb://localhost/bigredhacks', {
     useMongoClient: true,
     /* other options */
@@ -18,8 +19,30 @@ function getContactInfo(callback) {
             console.log("Starting user dump.");
             // let stream = fs.createWriteStream("participant_info.csv");
             let result = [];
-            result.push("First Name,Last Name,Email,Major,Gender,Year\r\n");
-            result = result.concat(users.map(user => user.name.first + "," + user.name.last + "," + user.email + "," + user.school.major + "," + user.gender + "," + user.school.year + "\r\n"));
+            result.push("First Name,Last Name,Email,Phone Number,Major,Gender,Year\r\n");
+            result = result.concat(users.map(user => user.name.first + "," + user.name.last + "," + user.email + "," + user.phone + "," + user.school.major + "," + user.gender + "," + user.school.year + "\r\n"));
+            console.log("Finished writing users: ");
+            console.log(result);
+            
+            callback(null, encodeURI(result.join("")));
+        }
+    });
+}
+
+// Also includes GitHub and LinkedIn
+function getContactInfoExtra(callback) {
+    let query = { "internal.status": "Accepted" };
+
+    User.find(query, null, { "name.first": 1 }, function (err, users) {
+        if (err) {
+            console.error("Error getting users.");
+        }
+        else {
+            console.log("Starting user dump.");
+            // let stream = fs.createWriteStream("participant_info.csv");
+            let result = [];
+            result.push("First Name,Last Name,Email,Phone Number,Major,Gender,Year,Resume,GitHub,LinkedIn\r\n");
+            result = result.concat(users.map(user => user.name.first + "," + user.name.last + "," + user.email + "," + user.phone + "," + user.school.major + "," + user.gender + "," + user.school.year + "," + (helper.s3url() + "/resume/" + user.app.resume) + "," + user.app.github + "," + user.app.linkedin + "\r\n"));
             console.log("Finished writing users: ");
             console.log(result);
             
@@ -35,7 +58,8 @@ function getContactInfo(callback) {
  */
 module.exports = (req, res, next) => {
     async.parallel({
-        contactInfo: getContactInfo
+        contactInfo: getContactInfo,
+        deluxeContactInfo: getContactInfoExtra
     }, function (err, result) {
         if (err) {
             console.error(err);
@@ -44,7 +68,8 @@ module.exports = (req, res, next) => {
 
         return res.render('admin/sponsors', {
             title: 'Admin Dashboard - Sponsors',
-            contactInfo: result.contactInfo
+            contactInfo: result.contactInfo,
+            deluxeContactInfo: result.deluxeContactInfo
         });
     });
 };
