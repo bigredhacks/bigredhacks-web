f088c75c804a3f2d5466c10edf76d15de71c83cd
Shea Belsky <sheabelsky@gmail.com>
2018-12-12 21:14:08 -0500

    2018 Updates

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,14 @@
 "use strict";
 
 // Node Modules and variables
-const express = require('express');
-const LocalStrategy = require('passport-local').Strategy;
-const middle = require('./middleware');
-const passport = require('passport');
+const express = require("express");
+const LocalStrategy = require("passport-local").Strategy;
+const middle = require("./middleware");
+const passport = require("passport");
 
 // Mongo models
-let Mentor = require('../models/mentor.js');
-let User = require('../models/user.js');
+let Mentor = require("../models/mentor.js");
+let User = require("../models/user.js");
 
 // Route imports
 const login           = require("./auth/login");
@@ -16,22 +16,25 @@ const passwordForgot  = require("./auth/passwordForgot");
 const passwordReset   = require("./auth/passwordReset");
 const register        = require("./auth/register");
 
-passport.use('user_strat', new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
+passport.use("user_strat", new LocalStrategy({
+    usernameField: "email",
+    passwordField: "password",
     passReqToCallback: true
 },
 function (req, email, password, done) {
     let sanitizedEmail = email ? email.trim().toLowerCase() : email;
     User.findOne({email: sanitizedEmail}, function (err, user) {
         if (err) {
+            console.log("err");
             return done(err);
         }
         if (user === null || !user.validPassword(password)) {
+            console.log("no user");
             return done(null, false, function () {
-                req.flash('error', 'Incorrect username or email.');
+                req.flash("error", "Incorrect username or email.");
             }());
         }
+        console.log("fine");
         return done(null, user);
     });
 }));
@@ -47,7 +50,8 @@ passport.deserializeUser(function (id, done) {
     User.findById(id, function (err, user) {
         if (user) {
             done(err, user);
-        } else {
+        }
+        else {
             Mentor.findById(id, function (err, user) {
                 done(err, user);
             });
@@ -59,22 +63,22 @@ module.exports = function (io) {
     let router = express.Router();
 
     // Login
-    router.get('/login',  login.get);
-    router.post('/login', passport.authenticate('user_strat', {
-        failureRedirect: '/login',
+    router.get("/login",  login.get);
+    router.post("/login", passport.authenticate("user_strat", {
+        failureRedirect: "/login",
         failureFlash: true
     }), login.post);
 
     // Forgot Password
-    router.get('/forgotpassword',  passwordForgot.get);
-    router.post('/forgotpassword', passwordForgot.post);
+    router.get("/forgotpassword",  passwordForgot.get);
+    router.post("/forgotpassword", passwordForgot.post);
 
     // Reset Password
-    router.get('/resetpassword',   passwordReset.get);
-    router.post('/resetpassword',  passwordReset.post);
+    router.get("/resetpassword",   passwordReset.get);
+    router.post("/resetpassword",  passwordReset.post);
 
     // Registration (General)
-    router.get('/register/:name?', (req, res, next) => {
+    router.get("/register/:name?", (req, res, next) => {
         if (req.params.name) {
             return middle.requireCornellRegistrationOpen(req, res, next);
         }
@@ -83,7 +87,7 @@ module.exports = function (io) {
         }
     }, middle.requireRegistrationOpen, middle.requireNoAuthentication, register.get);
 
-    router.post('/register/:name?', (req, res, next) => {
+    router.post("/register/:name?", (req, res, next) => {
         if (req.params.name) {
             return middle.requireCornellRegistrationOpen(req, res, next);
         }
