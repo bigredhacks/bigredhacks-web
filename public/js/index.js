dce72b692844a301c5b394502adcf10098852ce6
Max Chu <maxmchu@gmail.com>
2019-05-28 17:02:49 -0400

    Splash and nav

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,26 @@
 // Get all divs with parallax needs
-let scenes = ['mainDiv', 'aboutDiv', 'faqDiv', 'sponsorsDiv'];
+// let scenes = ['mainDiv', 'aboutDiv', 'faqDiv', 'sponsorsDiv'];
 
-// Start parallax
-scenes.forEach(scene => {
-    let el = document.getElementById(scene);
-    let parallax = new Parallax(el);
+// // Start parallax
+// scenes.forEach(scene => {
+//     let el = document.getElementById(scene);
+//     let parallax = new Parallax(el);
+// });
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var scrollPos = $(document).scrollTop();
+        $('.nav-item').each(function () {
+            var currElmt = $(this);
+            var currLink = $(this).find("a");
+            var refElement = $(currLink.attr("href"));
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                $('.nav-item').removeClass("active");
+                currElmt.addClass("active");
+            }
+            else {
+                currElmt.removeClass("active");
+            }
+        });
+    });
 });
